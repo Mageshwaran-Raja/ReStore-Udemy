b28df55643800ac84365547559729fb682f6a331
mageshwaran16 <mageshwaran.raja@kanini.com>
2023-04-14 19:06:16 +0530

    End of Section 4

--- client/src/features/catelog/Catelog.tsx
+++ client/src/features/catelog/Catelog.tsx
@@ -1,15 +1,16 @@
 import { Product } from "../../app/models/Product";
 import ProductList from "./ProductList";
 import { useEffect, useState } from "react";
+import  axios from 'axios';
 
 export default function Catelog() {
 
     const [products, setProduct] = useState<Product[]>([]);
 
     useEffect(() => {
-      fetch("http://localhost:5000/api/Products")
-        .then(response => response.json())
-        .then(data => setProduct(data));
+      axios.get("http://localhost:5000/api/Products")
+        .then(response => setProduct(response.data))
+        .catch(error => console.log(error));
     }, [])
 
     return (
--- client/src/features/catelog/ProductDetail.tsx
+++ client/src/features/catelog/ProductDetail.tsx
@@ -1,7 +1,63 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Product } from "../../app/models/Product";
+import axios from "axios";
 
 export default function ProductDetail(){
+
+    const {id} = useParams<{id: string}>();
+    const [product, setProduct] = useState<Product | null>(null);
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/Products/${id}`)
+            .then(response => setProduct(response.data))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [id]);
+
+    if(loading) return <h1>Loading...</h1>
+
+    if(!product) return <h3>Product Not Found</h3>
+
     return(
-        <Typography variant='h2'>Product Detail</Typography>
+        <Grid container spacing={6}>
+            <Grid item xs={6}>
+                <img src={product.pictureUrl} alt={product.name} style={{width: '100%'}} />
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant='h3' color='secondary'>{product.name}</Typography>
+                <Divider sx={{md: 2}} />
+                <Typography variant='h4' color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{product.brand}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Type</TableCell>
+                                <TableCell>{product.type}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity In Stock</TableCell>
+                                <TableCell>{product.quantityInStock}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
     )
 }
